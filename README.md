a4f59e012bce122ef4b0384b602a7713fc6ade4f
jb747 <108913053+jb747@users.noreply.github.com>
2024-02-21 12:14:27 -0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# sse-client
+
+A client for server-sent events published by sse-proto.
+
+To run: `npm start`.
+To deploy to Cloudflare: `npm run deploy`
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [facts, setFacts] = useState([]);
+  const [listening, setListening] = useState(false);
+
+  useEffect(() => {
+    if (!listening) {
+      // this works when the server is running locally
+      // const events = new EventSource('http://localhost:8787/events');
+      const events = new EventSource('https://sse-proto.james-fremen.workers.dev');
+
+      events.onmessage = (event) => {
+        console.log(`Stringified event: ${JSON.stringify(event)}`)
+        console.log(`Received event: ${event.data}`)
+        const parsedData = JSON.parse(event.data);
+        console.log(`parsedData: ${JSON.stringify(parsedData)}`)
+
+        setFacts((facts) => facts.concat(parsedData));
+      };
+
+      setListening(true);
+    }
+  }, [listening, facts]);
+
+  return (
+    <table className="stats-table">
+      <thead>
+        <tr>
+          <th>Fact</th>
+          <th>Source</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          facts.map((fact, i) =>
+            <tr key={i}>
+              <td>{fact.info}</td>
+              <td>{fact.source}</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
